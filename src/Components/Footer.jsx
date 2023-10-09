24a5637eec09811ee31792519bf9da5205ceb55c
NolanCST <nolan.costa06@gmail.com>
2023-10-09 13:52:23 +0200

    Changement footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,7 @@ function Footer() {
    return (
       <>
          <div className="containerFooter">
-            <p>Toutes reprise non autorisée de ce site internet sera soumis a une amende exessivement chère qui me permettra d'investir dans l'immobilier</p>
-            <p>&copy; Nono le plus bô</p>
+            <p>&copy; NolanCST</p>
          </div>
       </>
    );
